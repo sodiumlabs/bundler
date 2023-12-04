034543feaa7c4ac37b82c96b29bbd5c5ae5271a3
AlbertHuang <r.albert.huang@gmail.com>
2023-12-04 20:30:40 +0800

    fix: verification gas

--- pkg/modules/checks/verificationgas.go
+++ pkg/modules/checks/verificationgas.go
@@ -12,7 +12,7 @@ import (
 // and the preVerificationGas is sufficiently high (enough to pay for the calldata gas cost of serializing
 // the UserOperation plus PRE_VERIFICATION_OVERHEAD_GAS).
 func ValidateVerificationGas(op *userop.UserOperation, ov *gas.Overhead, maxVerificationGas *big.Int) error {
-	if op.VerificationGasLimit.Cmp(maxVerificationGas) > 0 {
+	if op.VerificationGasLimit.Cmp(maxVerificationGas) > 0 && len(op.InitCode) == 0 {
 		return fmt.Errorf(
 			"verificationGasLimit: exceeds maxVerificationGas of %s",
 			maxVerificationGas.String(),
